e833d7996e863f1808f8a238dd7a79a73e168af6
Peter Wilson <git-commit-by@peterwilson.cc>
2025-03-17 09:22:29 +1100

    Filter twemoji source.

--- inc/namespace.php
+++ inc/namespace.php
@@ -10,12 +10,38 @@
 
 namespace PWCC\LocalTwemoji;
 
+use PWCC\LocalTwemoji\Generated;
+
 /**
  * Bootstrap the plugin.
  */
 function bootstrap() {
 	add_filter( 'emoji_url', __NAMESPACE__ . '\\filter_emoji_url' );
 	add_filter( 'emoji_svg_url', __NAMESPACE__ . '\\filter_emoji_svg_url' );
+	add_filter( 'script_loader_src', __NAMESPACE__ . '\\filter_script_loader_src', 10, 2 );
+}
+
+/**
+ * Filters the script loader source.
+ *
+ * @param string $src    Script loader source path.
+ * @param string $handle Script handle.
+ * @return string Script loader source path.
+ */
+function filter_script_loader_src( $src, $handle ) {
+	if ( 'twemoji' !== $handle ) {
+		return $src;
+	}
+
+	// Get the URL for the JavaScript file.
+	$plugin_url = plugin_dir_url( __DIR__ );
+	$src        = $plugin_url . 'js/twemoji.js';
+
+	$version = Generated\get_twemoji_version();
+	if ( $version ) {
+		$src = add_query_arg( 'ver', $version, $src );
+	}
+	return $src;
 }
 
 /**
