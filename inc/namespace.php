84031d82c7d52e6e7aeb52528f7cec9c88550e9d
Peter Wilson <git-commit-by@peterwilson.cc>
2025-06-23 09:43:59 +1000

    Add cache busting query string parameter.
    Adds a cache busting query string parameter to the extensions to account for sites with long cache times on asset URLs. This ensures that updates to emoji are propergated following an update to the plugin.

--- inc/namespace.php
+++ inc/namespace.php
@@ -18,6 +18,9 @@ const PLUGIN_VERSION = '1.0.0';
 function bootstrap() {
 	add_filter( 'emoji_url', __NAMESPACE__ . '\\filter_emoji_url' );
 	add_filter( 'emoji_svg_url', __NAMESPACE__ . '\\filter_emoji_svg_url' );
+
+	add_filter( 'emoji_ext', __NAMESPACE__ . '\\extension_cache_busting' );
+	add_filter( 'emoji_svg_ext', __NAMESPACE__ . '\\extension_cache_busting' );
 }
 
 /**
@@ -28,7 +31,7 @@ function bootstrap() {
 function filter_emoji_url() {
 	// Get the URL for the plugin.
 	$plugin_url = plugin_dir_url( __DIR__ );
-	return $plugin_url . 'images/emoji/72x72/';
+	return sanitize_url( $plugin_url . 'images/emoji/72x72/' );
 }
 
 /**
@@ -37,5 +40,31 @@ function filter_emoji_url() {
 function filter_emoji_svg_url() {
 	// Get the URL for the plugin.
 	$plugin_url = plugin_dir_url( __DIR__ );
-	return $plugin_url . 'images/emoji/svg/';
+	return sanitize_url( $plugin_url . 'images/emoji/svg/' );
+}
+
+/**
+ * Add cache-busting query strings to emoji URLs.
+ *
+ * This is to ensure that browsers do not serve cached versions
+ * of emoji images following an update to the plugin.
+ *
+ * The URL of the images does not change, so the plugin version
+ * is appended as a query string parameter.
+ *
+ * @param string $ext The file extension of the emoji, either .svg or .png.
+ * @return string The extension with the plugin version appended as a query string.
+ */
+function extension_cache_busting( $ext ) {
+	/*
+	 * Append the plugin version to the extension as a query string.
+	 *
+	 * As the extension is not a full URL it's not possible to use
+	 * `sanitize_url()` here, so we use `sanitize_title()` to ensure
+	 * that:
+	 *
+	 * - dots are converted to hyphens
+	 * - typos are converted/removed to ensure a valid string
+	 */
+	return $ext . '?ver=' . sanitize_title( PLUGIN_VERSION );
 }
